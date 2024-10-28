ad38892a87440b33c315a39e8a9729cf71267b95
NAV24\uesnav <6441435248tlc@gmail.com>
2024-10-28 14:26:00 -0700

    Final 28/10/2024

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-<xcontador></xcontador>
+  <MainView></MainView>
+<contador></contador>
+<lista-de-tareas></lista-de-tareas>
 </template>
 
 <script setup>
-import Contador from './components/Contador.vue';
+import MainView from './views/mainView.vue';
 </script>
 
 <style scoped>
